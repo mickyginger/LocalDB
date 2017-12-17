0c34fd2102cc59dba73d105e596f0daef60b3a94
mickyginger <mickyginger@gmail.com>
2017-12-17 15:59:31 +0000

    Added findOne and drop

--- src/index.js
+++ src/index.js
@@ -1,78 +1,86 @@
-var sift = require('sift')
+var sift = require('sift');
+var ObjectId = require('bson').ObjectID;
 
 var LocalDB = function(tableName){
   var getTable = function(name){
     var data;
-    if(!localStorage[name]) localStorage[name] = '[]'
+    if(!localStorage[name]) localStorage[name] = '[]';
     try{
-      data = JSON.parse(localStorage[name])
+      data = JSON.parse(localStorage[name]);
+    } catch(e){
+      this.debug && console.info('Table ' + name + ' is broken creating new. All data is lost.');
+      localStorage[name] = '[]';
+      data = [];
     }
-    catch(e){
-      console.info('Table ',name,' is broken creating new. All data is lost')
-      localStorage[name] = '[]'
-      data = []
-    }
-    return data
-  }
-  getTable(tableName)
+    return data;
+  };
+
+  getTable(tableName);
+
   var self = this;
   this.debug = false;
-  this.update = function(query,item){
-    var Table = getTable(tableName)
-    sift(query,Table).forEach(function(el){
-      var ElementIndex = Table.indexOf(el)
-      item.id = el.id
-      Table[ElementIndex] = item
-    })
-    localStorage[tableName] = JSON.stringify(Table)
-  }
+
+  this.update = function(query, item){
+    var table = getTable(tableName);
+    var records = sift(query, table);
+    records.forEach(function(el){
+      var recordIndex = table.indexOf(el);
+      item.id = el.id;
+      table[recordIndex] = item;
+    });
+    this.debug && console.log(records.length + ' record(s) updated', records);
+    localStorage[tableName] = JSON.stringify(table);
+  };
+
   this.insert = function(object){
     if(object.id) {
-      var dbObject = self.query({
-        id: object.id
-      });
-      if(dbObject.length) {
-        self.update({id:object.id},object)
+      var record = self.query({ id: object.id });
+      if(record.length) {
+        self.update({ id: object.id }, object);
         // ok we did what we came for, go home
         return;
       }
     }
 
     // we either don't have object with object.id or object doesn't have id
-    var Table = getTable(tableName)
-    object.id = object.id || guid() // respect user's id
-    Table.push(object)
-    localStorage[tableName] = JSON.stringify(Table)
+    var table = getTable(tableName);
+    record.id = record.id || new ObjectId(); // respect user's id
+    table.push(record);
+    localStorage[tableName] = JSON.stringify(table);
+    this.debug && console.log('Record inserted', record);
+  };
 
-  }
   this.remove = function(query){
-    var Table = getTable(tableName)
-    sift(query,Table).forEach(function(el){
-      var ElementIndex = Table.indexOf(el)
-      Table.splice(ElementIndex,1)
-    })
-    localStorage[tableName] = JSON.stringify(Table)
-  }
-  this.query = function(q){
+    var table = getTable(tableName);
+    const records = sift(query, table);
+    records.forEach(function(el) {
+      var elementIndex = table.indexOf(el);
+      table.splice(elementIndex, 1);
+    });
+    localStorage[tableName] = JSON.stringify(table);
+    this.debug && console.log(records.length + ' records deleted', records);
+  };
+
+  this.find = function(q){
     if(typeof q !== 'object') {
       q = {};
     }
-    var result = sift(q,JSON.parse(localStorage[tableName]));
-    this.debug && console.table(result);
-    return result;
-  }
+    var records = sift(q, JSON.parse(localStorage[tableName]));
+    this.debug && console.table(records.length + ' record(s) found', records);
+    return records;
+  };
 
-}
+  this.findOne = function(q) {
+    const records = this.find(q);
+    return (records instanceof Array) ? records[0] : records;
+  };
+
+  this.drop = function() {
+    localStorage[name] = '[]';
+    this.debug && console.log('Table ' + name + ' dropped.');
+  };
+
+};
 // Export to global scope if running on the browser
-module.exports = LocalDB
-if(window) window.DB = LocalDB
-
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-}
+module.exports = LocalDB;
+if(window) window.DB = LocalDB;
